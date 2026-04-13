01a3e27832a503024b03831c5c2aaf83f4a757eb
Ruby <rubyomokhafe@gmail.com>
2026-04-12 22:27:11 -0400

    Refactor rock-paper-scissors game loop

--- rps.py
+++ rps.py
@@ -3,26 +3,30 @@
 #   https://realpython.com/python-rock-paper-scissors/
 
 import random
+play_again = "y"
 
-user_action = input("Enter throw (rock, paper, scissors): ")
-ai_action = random.choice(["rock", "paper", "scissors"])
-
-print(f"\nYou chose {user_action}, AI chose {ai_action}.\n")
-
-if user_action == ai_action:
-    print(f"Both players selected {user_action}. It's a tie!")
-elif user_action == "rock":
-    if ai_action == "scissors":
-        print("Rock smashes scissors! You win!")
-    else:
-        print("Paper covers rock! You lose.")
-elif user_action == "paper":
-    if ai_action == "paper":
-        print("Paper covers rock! You win!")
-    else:
-        print("Scissors cuts paper! You lose.")
-elif user_action == "scissors":
-    if ai_action == "paper":
-        print("Scissors cuts paper! You win!")
-    else:
+while play_again.lower() == "y": 
+    user_action = input("Enter throw (rock, paper, scissors): ")
+    ai_action = random.choice(["rock", "paper", "scissors"])
+    
+    print(f"\nYou chose {user_action}, AI chose {ai_action}.\n")
+    
+    if user_action == ai_action:
+        print(f"Both players selected {user_action}. It's a tie!")
+    elif user_action == "rock":
+        if ai_action == "scissors":
+            print("Rock smashes scissors! You win!")
+        else:
+            print("Paper covers rock! You lose.")
+    elif user_action == "paper":
+        if ai_action == "rock":
+            print("Paper covers rock! You win!")
+        else:
+            print("Scissors cuts paper! You lose.")
+    elif user_action == "scissors":
+        if ai_action == "paper":
+            print("Scissors cuts paper! You win!")
+        else:
         print("Rock smashes scissors! You lose.")
+
+    play_again = input("\nPlay again? (y/n): ")
